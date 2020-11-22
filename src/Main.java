1f7e2876c9eaf70f382fd10028ad431a016ef293
Rizal Herliansyah Hidayat <39128288+Rizalmultimedia2@users.noreply.github.com>
2020-11-22 21:22:59 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,12 @@ import javax.swing.SwingUtilities;
 
 public class Main{
     public static void main(String[] args) {
-        if(args.length != 0){
+        if(args.length > 1){
             int n = Integer.parseInt(args[0]);
             FramePuzzle frame = new FramePuzzle(n);
             SwingUtilities.invokeLater(frame);
+          } else if (args.length <= 1){
+            System.out.println("Maaf ukurannya tidak boleh <= 1 !");
           } else {
             FramePuzzle frame = new FramePuzzle(4);
             SwingUtilities.invokeLater(frame);
