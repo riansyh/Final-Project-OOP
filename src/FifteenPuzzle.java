e240ca893a83baa3c96fe360c408a706d469a489
Rian Febriansyah <rianfebriansyah22@gmail.com>
2020-11-22 11:33:39 +0700

    Add FifteenPuzzle class

---
+++ src/FifteenPuzzle.java
@@ -0,0 +1,7 @@
+public class FifteenPuzzle{
+    int[] ubin;
+
+    public FifteenPuzzle(){
+        
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,5 @@
+pubic class Main{
+    public static void main(String[] args) {
+        
+    }
+}
