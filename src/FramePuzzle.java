57bc1af6b3b467777ab47be67368b5256bb7df80
Rizal Herliansyah Hidayat <39128288+Rizalmultimedia2@users.noreply.github.com>
2020-11-22 20:50:01 +0700

    Update FramePuzzle.java

--- src/FramePuzzle.java
+++ src/FramePuzzle.java
@@ -4,16 +4,16 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
-public class FramePuzzle implements Runnable{
+public class FramePuzzle implements Runnable {
     private JFrame frame;
-    
+
     @Override
     public void run() {
         frame = new JFrame("Game Fifteen Puzzle");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setResizable(false);
         createComponents(frame.getContentPane());
-        frame.add(new FifteenPuzzle(), BorderLayout.CENTER);
+        frame.add(new FifteenPuzzle(4, 550, 30), BorderLayout.CENTER);
         frame.pack();
         frame.setVisible(true);
     }
@@ -24,5 +24,5 @@ public class FramePuzzle implements Runnable{
 
     public JFrame getFrame() {
         return frame;
-    }    
+    }
 }
