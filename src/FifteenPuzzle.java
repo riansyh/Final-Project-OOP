c1c4a2c1f75b397e7748d089a74c1bdb4d6d6398
Rian Febriansyah <rianfebriansyah22@gmail.com>
2020-11-29 15:57:06 +0700

    Update FifteenPuzzle.java

--- src/FifteenPuzzle.java
+++ src/FifteenPuzzle.java
@@ -18,8 +18,7 @@ public class FifteenPuzzle extends JPanel {
     private int size;
     private int dimension;
     private int nUbin;
-    private int blankPos;
-    private static final Color FOREGROUND_COLOR = new Color(239, 83, 80);
+    private int blank;
     private static final Random RANDOM = new Random();
     private int[] ubin;
     private int margin;
@@ -32,10 +31,72 @@ public class FifteenPuzzle extends JPanel {
         this.size = size;
         this.dimension = dimension;
         this.margin = margin;
+        this.gameOver = true;
+
+        this.nUbin = size * size - 1;
+        this.ubin = new int[size * size];
+
+        this.gridSize = (dimension - (2 * margin));
+        this.sizeUbin = this.gridSize / size;
+
         setPreferredSize(new Dimension(dimension, dimension + margin));
-        setBackground(Color.LIGHT_GRAY);
+        setBackground(Color.WHITE);
         setForeground(Color.ORANGE);
         setFont(new Font("SansSerif", Font.BOLD, 40));
+
+        addMouseListener(new MouseAdapter(){
+            @Override
+            public void mousePressed(MouseEvent mouse) {
+                if(gameOver){
+                    newGame();
+                } else {
+                    int clickX = mouse.getX() - margin;  
+                    int clickY = mouse.getY() - margin;
+
+                    if (clickX < 0 || clickY < 0 || clickX > gridSize || clickY > gridSize ) {
+                        return;
+                    }
+
+                    int posisiX = clickX / sizeUbin; //c1
+                    int posisiY = clickY / sizeUbin; //r1
+
+                    int blankX = blank % size; //c2
+                    int blankY = blank / size; //r2
+
+                    int posisiArray = posisiY * size + posisiX;
+
+                    int arah = 0;
+
+                    if(posisiX == blankX && Math.abs(posisiY - blankY) > 0){
+                        if (posisiY - blankY > 0){
+                            arah = size;
+                        } else {
+                            arah = -size;
+                        }
+                    } else if (posisiY == blankY && Math.abs(posisiX - blankX) > 0){
+                        if (posisiX - blankX > 0){
+                            arah = 1;
+                        } else {
+                            arah = -1;
+                        }                        
+                    }
+
+                    if (arah != 0){
+                        do{
+                            int newBlank = blank + arah;
+                            ubin[blank] = ubin[newBlank];
+                            blank = newBlank;
+                        } while (blank != posisiArray);
+
+                        ubin[blank] = 0;
+                    }
+
+                    gameOver = isSolved();
+                }
+                repaint();
+            }
+        });            
+        newGame();
     }
 
     private void newGame() {
@@ -50,7 +111,7 @@ public class FifteenPuzzle extends JPanel {
         for (int i = 0; i < ubin.length; i++) {
             ubin[i] = (i + 1) % ubin.length;
         }
-        blankPos = ubin.length - 1;
+        blank = ubin.length - 1;
     }
 
     private void shuffle() {
@@ -85,30 +146,27 @@ public class FifteenPuzzle extends JPanel {
         return true;
     }
 
-
-
     private void drawGrid(Graphics2D g) {
         for (int i = 0; i < ubin.length; i++) {
 
-            int r = i / size;
-            int c = i % size;
+            int r = i / this.size;
+            int c = i % this.size;
 
-            int x = margin + c * sizeUbin;
-            int y = margin + r * sizeUbin;
+            int x = this.margin + c * this.sizeUbin;
+            int y = this.margin + r * this.sizeUbin;
 
             if (ubin[i] == 0) {
                 if (gameOver) {
-                    g.setColor(FOREGROUND_COLOR);
-                    drawCenteredString(g, "\u2713", x, y);
+                    g.setColor(Color.ORANGE);
+                    drawCenteredString(g, "SELAMAT!", x, y);
                 }
-
                 continue;
             }
 
             g.setColor(getForeground());
-            g.fillRoundRect(x, y, sizeUbin, sizeUbin, 25, 25);
+            g.fillRect(x, y, sizeUbin, sizeUbin);
             g.setColor(Color.BLACK);
-            g.drawRoundRect(x, y, sizeUbin, sizeUbin, 25, 25);
+            g.drawRect(x, y, sizeUbin, sizeUbin);
             g.setColor(Color.WHITE);
 
             drawCenteredString(g, String.valueOf(ubin[i]), x, y);
@@ -118,7 +176,7 @@ public class FifteenPuzzle extends JPanel {
     private void drawStartMessage(Graphics2D g) {
         if (gameOver) {
             g.setFont(getFont().deriveFont(Font.BOLD, 18));
-            g.setColor(FOREGROUND_COLOR);
+            g.setColor(Color.ORANGE);
             String s = "Click to start new game";
             g.drawString(s, (getWidth() - g.getFontMetrics().stringWidth(s)) / 2, getHeight() - margin);
         }
