7552877118eccf0787fb1fe4851b3453a9024155
Rian Febriansyah <rianfebriansyah22@gmail.com>
2020-12-06 23:01:21 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,11 @@
+/*
+| NPM          | Name                |
+| ------------ | ------------------- |
+| 140810190002 | Rizal Herliansyah H |
+| 140810190014 | Indra Kurniawan     |
+| 140810190026 | Rian Febriansyah    |
+*/
+
 import javax.swing.SwingUtilities;
 
 public class Main{
@@ -17,4 +25,3 @@ public class Main{
         
     }
 }
-
